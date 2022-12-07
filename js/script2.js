5b2395fc97c946cf917c78de5203947046819c5b
unknown <dollorinfotech@gmail.com>
2022-12-07 09:00:21 +0530

    commit 2

---
+++ js/script2.js
@@ -0,0 +1,16 @@
+
+
+let news1 = {
+    //1. Property:Value
+    w1:"Amid",
+    w2:'clash',
+    w3:`between`
+};
+
+
+//1. Console.log
+
+let word3 = "w3";
+console.log(news1.w1+' '+news1["w2"]+" "+news1[word3]);
+
+//window.alert(news1.w1);
